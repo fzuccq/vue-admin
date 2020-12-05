494c0e581ed6fff1fe8a59ac729c6bb0cfb638ba
1024512 <fzuccq@163.com>
2020-12-05 16:00:51 +0800

    登录页验证,axios配置,请求验证码接口

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -17,6 +17,7 @@
         <el-form-item prop="username" class="form-item">
           <label for="username">账号</label>
           <el-input
+            id="username"
             type="text"
             v-model="ruleForm.username"
             autocomplete="off"
@@ -25,6 +26,7 @@
         <el-form-item prop="pass" class="form-item">
           <label for="pass">密码</label>
           <el-input
+            id="pass"
             type="password"
             v-model="ruleForm.pass"
             autocomplete="off"
@@ -35,6 +37,7 @@
         <el-form-item prop="checkPass" class="form-item" v-if="register">
           <label for="checkPass">重复密码</label>
           <el-input
+            id="checkPass"
             type="password"
             v-model="ruleForm.checkPass"
             autocomplete="off"
@@ -46,12 +49,12 @@
           <label for="validCode">验证码</label>
           <el-row :gutter="10">
             <el-col :span="14"
-              ><el-input v-model.number="ruleForm.validCode"></el-input
+              ><el-input id="validCode" v-model.number="ruleForm.validCode"></el-input
             ></el-col>
             <el-col :span="10"
               ><el-button
                 type="success"
-                @click="submitForm('ruleForm')"
+                @click="btnGetSms()"
                 class="block"
                 >获取验证码</el-button
               ></el-col
@@ -63,7 +66,8 @@
             type="danger"
             @click="submitForm('ruleForm')"
             class="login-btn block"
-            >提交</el-button
+            :disabled="loginButtonState"
+            >{{register?"注册":"登录"}}</el-button
           >
         </el-form-item>
       </el-form>
@@ -71,11 +75,62 @@
   </div>
 </template>
 <script>
+import {getSms} from '@/api/login';
+import { reactive,ref,onMounted, computed } from '@vue/composition-api'
 import {validateEmail,validatePassword,validateVCode,stripscript} from '@/utils/validate';
 export default {
   name: "login",
-  data() {
-    var menutabs = [
+  setup(props,{refs,root}){
+    let validateUserName = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入用户名"));
+      } else if(!validateEmail(value)) {
+        callback(new Error("用户名格式有误！"));
+      } else {
+        callback();
+      }
+    };
+
+    let validatePass = (rule, value, callback) => {
+      ruleForm.pass=stripscript(value);
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else if(!validatePassword(value)){
+        callback(new Error("密码为6至20位数字+字母"));
+      } else {
+        if (ruleForm.checkPass !== "") {
+          refs.ruleForm.validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if(!register) {callback();}
+      ruleForm.checkPass=stripscript(value);
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (ruleForm.checkPass !== ruleForm.pass) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
+
+    let validateCode=(rule,value,callback)=>{
+      if (value === "") {
+        callback(new Error("请输入验证码"));
+      } else if (!validateVCode(value)) {
+        callback(new Error("验证码格式有误！"));
+      } else {
+        callback();
+      }
+      
+    };
+
+    const loginButtonState=ref(true);
+
+    // data数据，生命周期和自定义函数
+    const menutabs=reactive([
       {
         text: "登录",
         type: "login",
@@ -86,77 +141,48 @@ export default {
         type: "register",
         selected: false,
       },
-    ];
+    ]);
 
-    var validateUserName = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请输入用户名"));
-      } else if(!validateEmail(value)) {
-        callback(new Error("用户名格式有误！"));
-      } else {
-        callback();
-      }
-    };
-    var validatePass = (rule, value, callback) => {
-      this.ruleForm.pass=stripscript(value);
-      if (value === "") {
-        callback(new Error("请输入密码"));
-      } else if(!validatePassword(value)){
-        callback(new Error("密码为6至20位数字+字母"));
-      } else {
-        if (this.ruleForm.checkPass !== "") {
-          this.$refs.ruleForm.validateField("checkPass");
-        }
-        callback();
-      }
-    };
-    var validatePass2 = (rule, value, callback) => {
-      if(!this.register) {callback();}
-      this.ruleForm.checkPass=stripscript(value);
-      if (value === "") {
-        callback(new Error("请再次输入密码"));
-      } else if (this.ruleForm.checkPass !== this.ruleForm.pass) {
-        callback(new Error("两次输入密码不一致!"));
-      } else {
-        callback();
-      }
-    };
+    const ruleForm= reactive({
+      username:"",
+      pass: "",
+      checkPass: "",
+      validCode: "",
+    });
 
-    var validateCode=(rule,value,callback)=>{
-      if (value === "") {
-        callback(new Error("请输入验证码"));
-      } else if (!validateVCode(value)) {
-        callback(new Error("验证码格式有误！"));
-      } else {
-        callback();
-      }
-      
-    }
+    const rules= reactive({
+      username: [{ validator: validateUserName, trigger: "blur" }],
+      pass: [{ validator: validatePass, trigger: "blur" }],
+      checkPass: [{ validator: validatePass2, trigger: "blur" }],
+      validCode: [{ validator: validateCode, trigger: "blur" }],
+    });
 
-    return {
-      menutabs: menutabs,
-      ruleForm: {
-        username:"",
-        pass: "",
-        checkPass: "",
-        validCode: "",
-      },
-      rules: {
-        username: [{ validator: validateUserName, trigger: "blur" }],
-        pass: [{ validator: validatePass, trigger: "blur" }],
-        checkPass: [{ validator: validatePass2, trigger: "blur" }],
-        validCode: [{ validator: validateCode, trigger: "blur" }],
-      },
-    };
-  },
-  methods: {
-    toggleMenutab(tab) {
-      this.menutabs.forEach((item, index) => (item.selected = false));
+    const toggleMenutab=(tab=> {
+      menutabs.forEach((item, index) => (item.selected = false));
       tab.selected = true;
-      this.resetForm("ruleForm");
-    },
-    submitForm(formName) {
-      this.$refs[formName].validate((valid) => {
+      resetForm("ruleForm");
+    });
+
+    const btnGetSms=(()=>{
+      if(!ruleForm.username){
+        root.$message({
+          message: '邮箱为空，不能获取验证码',
+          type: 'warning'
+        });
+        return false;
+      }
+      if(!validateEmail(ruleForm.username)){
+        root.$message({
+          message: '邮箱格式有误！！',
+          type: 'warning'
+        });
+        return false;
+      }
+      getSms({username:ruleForm.username,module:'login'});
+    });
+
+    const submitForm=(formName=> {
+      refs[formName].validate((valid) => {
         if (valid) {
           alert("submit!");
         } else {
@@ -164,24 +190,62 @@ export default {
           return false;
         }
       });
-    },
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
-    },
-  },
+      
+    });
 
-  computed:{
-    register(){
+    const resetForm=(formName=> {
+      refs[formName].resetFields();
+    });
+
+    const register=computed({get:()=>{
       var isRegister=false;
-      this.menutabs.forEach((item,index)=>{
+      menutabs.forEach((item,index)=>{
         if(item.selected && item.type==="register"){
           isRegister=true;
         }
       });
-
+ 
       return isRegister;
-    }
-  }
+    }});
+    
+    // 生命周期
+    onMounted(()=>{
+
+    });
+
+    return {
+      loginButtonState,
+      menutabs,
+      ruleForm,
+      rules,
+      toggleMenutab,
+      submitForm,
+      btnGetSms,
+      resetForm,
+      register
+    };
+
+  },
+  // methods: {
+  //   toggleMenutab(tab) {
+  //     this.menutabs.forEach((item, index) => (item.selected = false));
+  //     tab.selected = true;
+  //     this.resetForm("ruleForm");
+  //   },
+  //   submitForm(formName) {
+  //     this.$refs[formName].validate((valid) => {
+  //       if (valid) {
+  //         alert("submit!");
+  //       } else {
+  //         console.log("error submit!!");
+  //         return false;
+  //       }
+  //     });
+  //   },
+  //   resetForm(formName) {
+  //     this.$refs[formName].resetFields();
+  //   },
+  // },
 };
 </script>
 <style lang="scss" scoped>
